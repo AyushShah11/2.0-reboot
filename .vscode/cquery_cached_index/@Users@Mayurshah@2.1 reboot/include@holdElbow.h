8df9bdf355db5c79f3bad3d696cb55986f4cd17e
mayur <mayushah1@yahoo.com>
2020-12-18 17:48:11 -0800

    wow

--- .vscode/cquery_cached_index/@Users@Mayurshah@2.1 reboot/include@holdElbow.h
+++ .vscode/cquery_cached_index/@Users@Mayurshah@2.1 reboot/include@holdElbow.h
@@ -6,23 +6,6 @@
 
 void holdElbow(int angle)
 {
-  encoderReset(elbowEncoder);
-  int TARGET = 50;
-  int Kp = 20;
-  int MAX_OUT = 65;
-  int error = 0;
-  int output = 0;
-
-  	 printf("encoderValue = %d", encoderGet(elbowEncoder));
-     error = TARGET - encoderGet(elbowEncoder);
-    output = Kp * error;
-    if(abs(error) < MAX_OUT){
-      elbowSet(output);
-    }
-    else{
-      elbowSet(output/abs(output)*MAX_OUT);
-    }
-
  printf("HoldElbow function called");
  }
 
--- include/holdShoulder.h
+++ include/holdShoulder.h
@@ -5,23 +5,6 @@
 #include "shoulder.h"
 void holdShoulder(int angle)
 {
-  encoderReset(shoulderEncoder);
-  int TARGET = 50;
-  int Kp = 20;
-  int MAX_OUT = 65;
-  int error = 0;
-  int output = 0;
-
-  	 printf("encoderValue = %d", encoderGet(shoulderEncoder));
-     error = TARGET - encoderGet(shoulderEncoder);
-    output = Kp * error;
-    if(abs(error) < MAX_OUT){
-      shoulderSet(output);
-    }
-    else{
-      shoulderSet(output/abs(output)*MAX_OUT);
-    }
-
  printf("HoldShoulder function called");
  }
 
